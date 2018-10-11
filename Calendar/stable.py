b6aefe5d7b3f3eba7c30f909b143815eee0bc8db
Ace <saadnaseer63@yahoo.de>
2018-10-11 13:31:07 +0200

    Update stable.py

--- Calendar/stable.py
+++ Calendar/stable.py
@@ -9,18 +9,6 @@ If you have any questions, feel free to open an issue at Github.
 Copyright by Ace-Laboratory
 """
 
-# url refers to the iCal url. It's the link you can copy when you click on
-# 'export' Calendar in Google or Yahoo (and many more online) Calendars
-
-# api-key refers to your openweathermap api key. It can be generated for free
-# when you sign up for an account and consists of a bunch of numbers and letters
-
-# location refers to the city you live in. You api key will be used to grab live
-# weather data for this city. Use the format below (city-name, country code)
-
-# week_starts_on refers to the day on which the week starts on in your country.
-# Choose between Monday and Sunday.
-
 """ To quickly get started, go to the settings.py file and fill in the necessary fields there"""
 
 from settings import *
