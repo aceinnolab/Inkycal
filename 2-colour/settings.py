cbd7b288273202972a45a404ca528c06e590e426
Ace <saadnaseer63@yahoo.de>
2018-10-11 13:32:02 +0200

    Create settings.py

---
+++ 2-colour/settings.py
@@ -0,0 +1,18 @@
+""" To quickly get started, fill in the following details:"""
+
+url = "https://calendar.google.com/calendar/ical/en.usa%23holiday%40group.v.calendar.google.com/public/basic.ics"
+api_key = "api-key"
+location = "Julich, DE"
+week_starts_on = "Monday"
+
+# url refers to the iCal url. It's the link you can copy when you click on
+# 'export' Calendar in Google or Yahoo (and many more online) Calendars
+
+# api-key refers to your openweathermap api key. It can be generated for free
+# when you sign up for an account and consists of a bunch of numbers and letters
+
+# location refers to the city you live in. You api key will be used to grab live
+# weather data for this city. Use the format below (city-name, country code)
+
+# week_starts_on refers to the day on which the week starts on in your country.
+# Choose between Monday and Sunday.
