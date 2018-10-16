487defdbb94d8ab44bf6659b4bcf81d479cbcc35
Ace <saadnaseer63@yahoo.de>
2018-10-16 16:28:09 +0200

    Preparing for unified version

--- Calendar/epdif.py
+++ Calendar/epdif.py
@@ -1,29 +1,3 @@
-##
- #  @filename   :   epdif.py
- #  @brief      :   EPD hardware interface implements (GPIO, SPI)
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     July 10 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- #
- 
 import spidev
 import RPi.GPIO as GPIO
 import time
@@ -57,8 +31,6 @@ def epd_init():
     GPIO.setup(DC_PIN, GPIO.OUT)
     GPIO.setup(CS_PIN, GPIO.OUT)
     GPIO.setup(BUSY_PIN, GPIO.IN)
-    SPI.max_speed_hz = 6250000 #was 2000000
-    SPI.mode = 0b00 #was 0b00
+    SPI.max_speed_hz = 2000000
+    SPI.mode = 0b00
     return 0;
-
-### END OF FILE ###
