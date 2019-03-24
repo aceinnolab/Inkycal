eda090f84c688adbf4185a83e18a6fd9b56185c6
Ace <saadnaseer63@yahoo.de>
2019-03-24 21:31:45 +0100

    Removed duplicate echo line
    Installing the E-Paper-Calendar Software for your display was displayed twice when updating. This has been fixed

--- Installer-with-debug.sh
+++ Installer-with-debug.sh
@@ -45,7 +45,6 @@ if [ "$option" = 1 ]; then
 	cp -r /home/pi/E-Paper-Calendar /home/pi/E-Paper-Calendar-old
 	sudo rm -r /home/pi/E-Paper-Calendar
 	echo "Updating now..."
-	echo -e "\e[1;36m"Installing the E-Paper-Calendar Software for your display"\e[0m"
         cd
     else
         echo -e "Could not find any settings.py file in /home/pi/E-Paper-Calendar/Calendar"
