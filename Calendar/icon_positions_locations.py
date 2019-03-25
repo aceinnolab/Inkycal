547669cbcadc25ad45a5ab32fbf3d237bd6810e5
Ace <saadnaseer63@yahoo.de>
2019-03-25 21:36:53 +0100

    path fixed

--- Calendar/icon_positions_locations.py
+++ Calendar/icon_positions_locations.py
@@ -9,7 +9,7 @@ from PIL import Image
 from settings import language
 im_open = Image.open
 
-path = '/home/pi/E-Paper-Master/Calendar/'
+path = '/home/pi/Calendar/Calendar/'
 wpath = path+'weather-icons/'
 mpath = path+'translations/'+language+'/months/'
 weekpath = path+'translations/'+language+'/week/'
