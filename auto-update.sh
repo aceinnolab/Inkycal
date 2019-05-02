3343cf2f104b8a4ba44eefde0167597fbb86b5fd
Ace <saadnaseer63@yahoo.de>
2019-05-02 19:35:26 +0200

    Syncing changes from new settings.py file

--- auto-update.sh
+++ auto-update.sh
@@ -9,11 +9,9 @@ in="/home/pi/settings.py.old"
 out="/home/pi/Inky-Calendar/Calendar/settings.py"
 
 # replace template iCalendar URLs with user-defined URLs
-# sed -n -e "/^ical_urls/r $in" -i -e "/^ical_urls/d" $out
 sed -n -e "/^ical_urls/r $in" -i -e "/^ical_urls/d" $out
 sed -n -e "/^rss_feeds/r $in" -i -e "/^rss_feeds/d" $out
 sed -n -e "/^update_interval/r $in" -i -e "/^update_interval/d" $out
-sed -n -e "/^additional_feature/r $in" -i -e "/^additional_feature/d" $out
 sed -n -e "/^api_key/r $in" -i -e "/^api_key/d" $out
 sed -n -e "/^location/r $in" -i -e "/^location/d" $out
 sed -n -e "/^week_starts_on/r $in" -i -e "/^week_starts_on/d" $out
@@ -23,5 +21,8 @@ sed -n -e "/^display_colours/r $in" -i -e "/^display_colours/d" $out
 sed -n -e "/^language/r $in" -i -e "/^language/d" $out
 sed -n -e "/^units/r $in" -i -e "/^units/d" $out
 sed -n -e "/^hours/r $in" -i -e "/^hours/d" $out
+sed -n -e "/^top_section/r $in" -i -e "/^top_section/d" $out
+sed -n -e "/^middle_section/r $in" -i -e "/^middle_section/d" $out
+sed -n -e "/^bottom_section/r $in" -i -e "/^bottom_section/d" $out
 
 echo -e 'All operations done'
