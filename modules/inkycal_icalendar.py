b90eac0ad533b622742738d1e3cb2978b7792a7d
Ace <saadnaseer63@yahoo.de>
2020-02-25 14:28:35 +0100

    Improved event handling
    Function fetch_events now has timeline parameter, specifying the timeline for events. Only events within this timeline will be returned in the form of a list.
    Addef fix for timeline end. Previously all day events on the last day of the month would not be shown. This has been fixed now.
    Minor code improvements for slightly better speed. Added recurring events will be printed directly on the console.

--- modules/inkycal_icalendar.py
+++ modules/inkycal_icalendar.py
@@ -19,66 +19,74 @@ if use_recurring_events == True:
   from dateutil.rrule import rrulestr, rruleset
   import re
 
-def fetch_events():
-  """Set timelines for filtering upcoming events"""
-  timezone = get_tz()
-  now = arrow.now(tz=timezone)
-  beginning_of_month = now.replace(days= - now.day +1)
-  near_future = now.replace(days= 30)
-  further_future = now.replace(days=40)
+def fetch_events(from_time, until_time, timezone = get_tz()):
+  """Get events from sepcified range. Input arrow objects!"""
+  
+  if type(from_time) != arrow.arrow.Arrow or type(until_time) != arrow.arrow.Arrow:
+    print('Received invalid time objects! Please check if they are arrow objects!')
+    raise TypeError
+  else:
+    timeline_start = from_time
+    timeline_end = until_time.replace(minutes=1)
 
   """Parse the iCalendars from the urls, fixing some known errors with ics"""
   calendars = [Calendar(fix_ical(url)) for url in ical_urls]
 
-  """Filter any upcoming events from all iCalendars and add them to a list"""
+  """Filter events which haven't ended from start of timeline and begin before
+     the end of the timeline"""
   upcoming_events = [events for ical in calendars for events in ical.events
-    if beginning_of_month  <= events.end <= further_future or
-    beginning_of_month <= events.begin <= near_future]
+      if (timeline_start <= events.begin <= timeline_end) and
+         (timeline_start <= events.end <= timeline_end)]
 
-  """Try to parse recurring events. This is clearly experimental! """
+  """Fix event timings of upcoming events"""
+  if upcoming_events:
+    for events in upcoming_events:
+      if events.duration.days >= 1:
+        events.end.replace(days =-2)
+        events.make_all_day
+      elif events.duration.days < 1:
+        events.end = events.end.to(timezone)
+        events.begin = events.begin.to(timezone)
+
+  """Try to parse all recurring events. In beta!"""
   if use_recurring_events == True:
-    for ical in calendars:
-      for events in ical.events:
-        event_str = str(events)
-        if re.search('RRULE:(.+?)\n', event_str):
-          if events.all_day and events.duration.days > 1:
-            events.end = events.end.replace(days=-2)
-          else:
-            events.end = events.end.to(timezone)
-            events.begin = events.begin.to(timezone)
-          try:
-            rule = re.search('RRULE:(.+?)\n', event_str).group(0)[:-2]
-            if re.search('UNTIL=(.+?);', rule) and not re.search('UNTIL=(.+?)Z;', rule):
-              rule = re.sub('UNTIL=(.+?);', 'UNTIL='+re.search('UNTIL=(.+?);', rule).group(0)[6:-1]+'T000000Z;', rule)
-            dates = rrulestr(rule, dtstart= events.begin.datetime).between(after= now.datetime, before = further_future.datetime)
+    recurring_events = [events for ical in calendars for events in ical.events
+                        if re.search('RRULE:(.+?)\n', str(events))]
 
-            if dates:
-              duration = events.duration
-              for date in dates:
-                cc = events.clone()
-                cc.end = arrow.get(date+duration)
-                cc.begin = arrow.get(date)
-                upcoming_events.append(cc)
-                #print("Added '{}' with new start at {}".format(cc.name, cc.begin.format('DD MMM YY')))
+    """Fix event timings of upcoming events"""
+    if recurring_events:
+      for events in recurring_events:
+        if events.duration.days >= 1:
+          events.end.replace(days =-2)
+          events.make_all_day
+        elif events.duration.days < 1:
+          events.end = events.end.to(timezone)
+          events.begin = events.begin.to(timezone)
 
-          except Exception as e:
-            print('Something went wrong while parsing recurring events')
-            pass
+        """Try getting recurrence dates of recurring events"""
+        try:
+          rule = re.search('RRULE:(.+?)\n', str(events)).group(0)[:-2]
+          if re.search('UNTIL=(.+?);', rule) and not re.search('UNTIL=(.+?)Z;', rule):
+            rule = re.sub('UNTIL=(.+?);', 'UNTIL='+re.search('UNTIL=(.+?);', rule).group(0)[6:-1]+'T000000Z;', rule)
+          dates = rrulestr(rule, dtstart= events.begin.datetime).between(after= timeline_start, before = timeline_end.datetime)
+
+          if dates:
+            duration = events.duration
+            for date in dates:
+              cc = events.clone()
+              cc.end = arrow.get(date+duration)
+              cc.begin = arrow.get(date)
+              upcoming_events.append(cc)
+              print("Added '{}' starting on {}".format(cc.name, cc.begin.format('DD MMM YY')))
+
+        except Exception as e:
+          print('Problematic re-event: ', events.name, rule)
+          pass
 
   """Sort events according to their beginning date"""
   def sort_dates(event):
     return event.begin
   upcoming_events.sort(key=sort_dates)
-
-  """Multiday events are displayed incorrectly; fix that"""
-  for events in upcoming_events:
-    if events.all_day and events.duration.days > 1:
-      events.end = events.end.replace(days=-2)
-      events.make_all_day()
-
-    if not events.all_day:
-      events.end = events.end.to(timezone)
-      events.begin = events.begin.to(timezone)
 
   """ The list upcoming_events should not be modified. If you need the data from
   this one, copy the list or the contents to another one."""
