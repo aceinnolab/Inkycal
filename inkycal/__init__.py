e79cd134e0a0a0fe0a0f05b02c4106eb300c30d7
Ace <saadnaseer63@yahoo.de>
2020-05-29 04:01:13 +0200

    Fixed imports

--- inkycal/__init__.py
+++ inkycal/__init__.py
@@ -1,10 +1,15 @@
-from inkycal.config import layout
-from inkycal.config import settings_parser
+# Settings and Layout
+from inkycal.config.layout import Layout
+from inkycal.config.settings_parser import Settings
 
-
+# All supported inkycal_modules
 import inkycal.modules.inkycal_agenda
 import inkycal.modules.inkycal_calendar
 import inkycal.modules.inkycal_weather
 import inkycal.modules.inkycal_rss
-#import inkycal.modules.inkycal_image
+# import inkycal.modules.inkycal_image
+# import inkycal.modules.inkycal_server
+
+# Main file
+from inkycal.main import Inkycal
 
