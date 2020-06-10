06a1da5f1f20a0b22ca7b1ea1b32bb269696f7f5
Ace <saadnaseer63@yahoo.de>
2020-06-10 15:12:44 +0200

    Fixed some tests, added a few more

---
+++ inkycal/tests/inkycal_rss_test.py
@@ -0,0 +1,22 @@
+import unittest
+from inkycal.modules import RSS
+
+rss = RSS(
+  #size
+  (400,400),
+
+  # config
+  {
+    'rss_urls': ['http://feeds.bbci.co.uk/news/world/rss.xml#']
+  }
+
+  )
+
+class inkycal_rss_test(unittest.TestCase):
+  def test_generate_image(self):
+    print('testing image generation')
+    rss.generate_image()
+
+
+if __name__ == '__main__':
+  unittest.main()
