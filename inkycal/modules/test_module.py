94f13e63c5503f5ad921c00fb1e061d5213c6509
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-07-25 13:22:46 +0200

    Code refactor on test_module.py

--- inkycal/modules/test_module.py
+++ inkycal/modules/test_module.py
@@ -14,7 +14,7 @@ Copyright by aceisace
 # Required for setting up this module
 from inkycal.modules.template import inkycal_module
 from inkycal.custom import *
-
+import sys
 
 #############################################################################
 #                           Built-in library imports
@@ -22,7 +22,6 @@ from inkycal.custom import *
 
 # Built-in libraries go here
 from random import shuffle
-
 
 #############################################################################
 #                         External library imports
@@ -32,11 +31,11 @@ from random import shuffle
 # use try...except ImportError to check if it has been installed
 # If it is not found, print a short message on how to install this dependency
 try:
-  import feedparser
+    import feedparser
 except ImportError:
-  print('feedparser is not installed! Please install with:')
-  print('pip3 install feedparser')
-
+    print('feedparser is not installed! Please install with:')
+    print('pip3 install feedparser')
+    sys.exit(1)  # Required module(s) not installed so exit the program
 
 #############################################################################
 #                         Filename + logging (do not remove)
@@ -47,6 +46,7 @@ filename = os.path.basename(__file__).split('.py')[0]
 logger = logging.getLogger(filename)
 logger.setLevel(level=logging.INFO)
 
+
 #############################################################################
 #                         Class setup
 #############################################################################
@@ -56,92 +56,91 @@ logger.setLevel(level=logging.INFO)
 # Avoid giving too long names to classes
 
 class Simple(inkycal_module):
-  """ Simple Class
-  Explain what this module does...
-  """
+    """
+    Simple Class
+    Explain what this module does...
+    """
+    # Initialise the class (do not remove)
+    def __init__(self, section_size: tuple, section_config: dict) -> None:
+        """
+        Initialize <module name> module
+        """
+        # Initialise this module via the inkycal_module template (required)
+        super().__init__(section_size, section_config)
 
-  # Initialise the class (do not remove)
-  def __init__(self, section_size, section_config):
-    """Initialize inkycal_rss module"""
+        # module name (required)
+        self.name = self.__class__.__name__
 
-    # Initialise this module via the inkycal_module template (required)
-    super().__init__(section_size, section_config)
+        # module specific parameters (optional)
+        self.do_something = True
 
-    # module name (required)
-    self.name = self.__class__.__name__
+        # give an OK message (optional)
+        print('{0} loaded'.format(self.name))
 
-    # module specific parameters (optional)
-    self.do_something = True
+    #############################################################################
+    #                 Validation of module specific parameters                  #
+    #############################################################################
 
-    # give an OK message (optional)
-    print('{0} loaded'.format(self.name))
+    def __validate(self) -> None:
+        """
+        Validate module-specific parameters
+        """
+        # Check the type of module-specific parameters
+        # This function is optional, but very useful for debugging.
 
-#############################################################################
-#                 Validation of module specific parameters                  #
-#############################################################################
+        # Here, we are checking if do_something (from init) is True/False
+        if not isinstance(self.do_something, bool):
+            print('do_something has to be a boolean: True/False')
 
-  def _validate(self):
-    """Validate module-specific parameters"""
-    # Check the type of module-specific parameters
-    # This function is optional, but very useful for debugging.
+    #############################################################################
+    #                       Generating the image                                #
+    #############################################################################
 
-    # Here, we are checking if do_something (from init) is True/False
-    if not isinstance(self.do_something, bool):
-      print('do_something has to be a boolean: True/False')
+    def generate_image(self) -> None:
+        """
+        Generate image for this module
+        """
+        # Define new image size with respect to padding (required)
+        im_width = int(self.width - (self.width * 2 * self.margin_x))
+        im_height = int(self.height - (self.height * 2 * self.margin_y))
+        im_size = im_width, im_height
 
+        # Use logger.info(), logger.debug(), logger.warning() to display
+        # useful information for the developer
+        logger.info('image size: {} x {} px'.format(im_width, im_height))
 
-#############################################################################
-#                       Generating the image                                #
-#############################################################################
+        # Create an image for black pixels and one for coloured pixels (required)
+        im_black = Image.new('RGB', size=im_size, color='white')
+        im_colour = Image.new('RGB', size=im_size, color='white')
 
-  def generate_image(self):
-    """Generate image for this module"""
+        #################################################################
+        #                    Your code goes here                        #
+        #################################################################
 
-    # Define new image size with respect to padding (required)
-    im_width = int(self.width - (self.width * 2 * self.margin_x))
-    im_height = int(self.height - (self.height * 2 * self.margin_y))
-    im_size = im_width, im_height
+        # Write/Draw something on the image
 
-    # Use logger.info(), logger.debug(), logger.warning() to display
-    # useful information for the developer
-    logger.info('image size: {} x {} px'.format(im_width, im_height))
+        #   You can use these custom functions to help you create the image:
+        # - write()               -> write text on the image
+        # - get_fonts()           -> see which fonts are available
+        # - get_system_tz()       -> Get the system's current timezone
+        # - auto_fontsize()       -> Scale the fontsize to the provided height
+        # - textwrap()            -> Split a paragraph into smaller lines
+        # - internet_available()  -> Check if internet is available
+        # - draw_border()         -> Draw a border around the specified area
 
-    # Create an image for black pixels and one for coloured pixels (required)
-    im_black = Image.new('RGB', size = im_size, color = 'white')
-    im_colour = Image.new('RGB', size = im_size, color = 'white')
+        # If these aren't enough, take a look at python Pillow (imaging library)'s
+        # documentation.
 
-    #################################################################
+        #################################################################
 
-    #                    Your code goes here                        #
-    
-    # Write/Draw something on the image
-
-    #   You can use these custom functions to help you create the image:
-    # - write()               -> write text on the image
-    # - get_fonts()           -> see which fonts are available
-    # - get_system_tz()       -> Get the system's current timezone
-    # - auto_fontsize()       -> Scale the fontsize to the provided height
-    # - textwrap()            -> Split a paragraph into smaller lines
-    # - internet_available()  -> Check if internet is available
-    # - draw_border()         -> Draw a border around the specified area
-
-    # If these aren't enough, take a look at python Pillow (imaging library)'s
-    # documentation.
-
-    
-    #################################################################
-
-    # Save image of black and colour channel in image-folder
-    im_black.save(images+self.name+'.png', 'PNG')
-    im_colour.save(images+self.name+'_colour.png', 'PNG')
+        # Save image of black and colour channel in image-folder
+        im_black.save(images + self.name + '.png', 'PNG')
+        im_colour.save(images + self.name + '_colour.png', 'PNG')
 
 
 # Check if the module is being run by itself
 if __name__ == '__main__':
-  print('running {0} in standalone mode'.format(filename))
-
-
-
+    print('running {0} in standalone mode'.format(filename))
 
 ################################################################################
 # Last steps
@@ -163,4 +162,3 @@ if __name__ == '__main__':
 # How do I now import my module?
 # from inkycal.modules import Class
 # Where Class is the name of the class inside your module (e.g. Simple)
-  
