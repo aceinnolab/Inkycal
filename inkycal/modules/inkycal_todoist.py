ee843782605d25f8bda31b56c1b3cf471ac44d0b
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-07-25 12:48:35 +0200

    code refactor on inkycal_todoist, added missing requirement

--- inkycal/modules/inkycal_todoist.py
+++ inkycal/modules/inkycal_todoist.py
@@ -9,25 +9,27 @@ Copyright by aceisace
 from inkycal.modules.template import inkycal_module
 from inkycal.custom import *
 
+import sys
+
 try:
-  import todoist
+    import todoist
 except ImportError:
-  print('todoist is not installed! Please install with:')
-  print('pip3 install todoist-python')
+    print('todoist is not installed! Please install with:')
+    print('pip3 install todoist-python')
+    sys.exit(1)  # Exit program since required module is not installed
+
 
 filename = os.path.basename(__file__).split('.py')[0]
 logger = logging.getLogger(filename)
 logger.setLevel(level=logging.ERROR)
 
-
 api = todoist.TodoistAPI('your api key')
 api.sync()
 
 # Print name of author
-print(api.state['user']['full_name']+'\n')
-
+print(api.state['user']['full_name'] + '\n')
 
 tasks = (task.data for task in api.state['items'])
 
-for _ in tasks:
-  print('task: {} is {}'.format(_['content'], 'done' if _['checked'] == 1 else 'not done'))
+for task in tasks:
+    print('task: {} is {}'.format(task['content'], 'done' if task['checked'] == 1 else 'not done'))
