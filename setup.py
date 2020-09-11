58d4fe23619f5025db1a5dd64b11d18146861207
Ace <saadnaseer63@yahoo.de>
2020-09-12 00:50:51 +0200

    synced packages from requirement file

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ __author_email__ = "aceisace63@yahoo.com"
 __url__ = "https://github.com/aceisace/Inky-Calendar"
 
 __install_requires__ = ['pyowm==2.10.0',                  # weather
-                        'Pillow>=7.0.0' ,                 # imaging
+                        'Pillow>=7.1.1' ,                 # imaging
                         'icalendar==4.0.6',               # iCalendar parsing
                         'recurring-ical-events==0.1.17b0',# parse recurring events
                         'feedparser==5.2.1',              # parse RSS-feeds
-                        'numpy>=1.18.3',                  # image pre-processing
+                        'numpy>=1.18.2',                  # image pre-processing
                         'arrow>=0.15.6',                  # time handling
                         'jsmin>=2.2.2'                    # Parsing jsonc file
                         ]
