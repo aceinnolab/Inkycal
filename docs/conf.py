7cfe5c809e10b68fe3fa408a57a4610677cfd2bf
Ace <saadnaseer63@yahoo.de>
2020-11-10 23:39:54 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-
+master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'inkycal'
