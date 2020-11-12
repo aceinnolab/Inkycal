95c15b4e14ec2ae9bac83f7edd6884c710c95c25
Ace <saadnaseer63@yahoo.de>
2020-11-12 21:20:48 +0100

    adding support for Image module

--- inkycal/modules/__init__.py
+++ inkycal/modules/__init__.py
@@ -3,5 +3,5 @@ from .inkycal_calendar import Calendar
 from .inkycal_weather import Weather
 from .inkycal_feeds import Feeds
 from .inkycal_todoist import Todoist
-#from .inkycal_image import Image
-#from .inkycal_server import Server
+from .inkycal_image import Inkyimage
+#from .inkycal_server import Inkyserver
