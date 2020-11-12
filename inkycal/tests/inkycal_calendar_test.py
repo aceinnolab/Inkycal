9980986a7edc5004837e237e077d5ce2c116923e
Ace <saadnaseer63@yahoo.de>
2020-11-12 10:14:57 +0100

    Improved testing algorithm
    Generalized code logic for testing. Added support for several test configs at once.

--- inkycal/tests/inkycal_calendar_test.py
+++ inkycal/tests/inkycal_calendar_test.py
@@ -1,15 +1,16 @@
 import unittest
 from inkycal.modules import Calendar as Module
 
-test = {
+tests = [
+{
   "position": 2,
   "name": "Calendar",
   "config": {
-    "size": [880,343],
+    "size": [800, 400],
     "week_starts_on": "Monday",
-    "show_events": "True",
+    "show_events": True,
     "ical_urls": "https://www.officeholidays.com/ics-fed/usa",
-    "ical_files": "",
+    "ical_files": None,
     "date_format": "D MMM",
     "time_format": "HH:mm",
     "padding_x": 10,
@@ -17,18 +18,72 @@ test = {
     "fontsize": 12,
     "language": "en"
     }
-}
-
-module = Module(test)
+},
+{
+  "position": 2,
+  "name": "Calendar",
+  "config": {
+    "size": [800, 400],
+    "week_starts_on": "Sunday",
+    "show_events": True,
+    "ical_urls": "https://www.officeholidays.com/ics-fed/usa",
+    "ical_files": None,
+    "date_format": "D MMM",
+    "time_format": "HH:mm",
+    "padding_x": 10,
+    "padding_y": 10,
+    "fontsize": 12,
+    "language": "en"
+    }
+},
+{
+  "position": 2,
+  "name": "Calendar",
+  "config": {
+    "size": [800, 400],
+    "week_starts_on": "Monday",
+    "show_events": False,
+    "ical_urls": "https://www.officeholidays.com/ics-fed/usa",
+    "ical_files": None,
+    "date_format": "D MMM",
+    "time_format": "HH:mm",
+    "padding_x": 10,
+    "padding_y": 10,
+    "fontsize": 12,
+    "language": "en"
+    }
+},
+{
+  "position": 2,
+  "name": "Calendar",
+  "config": {
+    "size": [800, 400],
+    "week_starts_on": "Monday",
+    "show_events": True,
+    "ical_urls": None,
+    "ical_files": None,
+    "date_format": "D MMM",
+    "time_format": "HH:mm",
+    "padding_x": 10,
+    "padding_y": 10,
+    "fontsize": 12,
+    "language": "en"
+    }
+},
+]
 
 class module_test(unittest.TestCase):
   def test_get_config(self):
-    print('getting data for web-ui')
-    module.get_config()
+    print('getting data for web-ui...', end = "")
+    Module.get_config()
+    print('OK')
 
   def test_generate_image(self):
-    print('testing image generation')
-    module.generate_image()
+    for test in tests:
+      print(f'test {tests.index(test)+1} generating image..', end="")
+      module = Module(test)
+      module.generate_image()
+      print('OK')
 
 if __name__ == '__main__':
   unittest.main()
