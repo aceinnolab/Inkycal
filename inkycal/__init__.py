077392c2f97e817fac4febca40825ab481ab69dd
Ace <saadnaseer63@yahoo.de>
2020-11-21 16:19:24 +0100

    Official support for Image and Jokes module

--- inkycal/__init__.py
+++ inkycal/__init__.py
@@ -7,7 +7,8 @@ import inkycal.modules.inkycal_calendar
 import inkycal.modules.inkycal_weather
 import inkycal.modules.inkycal_feeds
 import inkycal.modules.inkycal_todoist
-# import inkycal.modules.inkycal_image
+import inkycal.modules.inkycal_image
+import inkycal.modules.inkycal_jokes
 # import inkycal.modules.inkycal_server
 
 # Main file
