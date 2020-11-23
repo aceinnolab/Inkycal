113e63898d93412694d088df7f8a1982ea18e9fc
Ace <saadnaseer63@yahoo.de>
2020-11-23 14:36:46 +0100

    switched theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,8 @@ release = '2.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxemoji.sphinxemoji']
-
+extensions = ['sphinx.ext.autodoc', 'sphinxemoji.sphinxemoji',
+              'sphinx_rtd_theme']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -46,13 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'classic' # removed classic theme
+html_theme = "sphinx_rtd_theme" # html_theme = 'classic' # removed classic theme
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#      html_static_path = ['_static']  # no static content required 
-
-html_theme = 'sphinx_hand_theme'
-import sphinx_hand_theme
-html_theme_path = [sphinx_hand_theme.get_html_theme_path()]
+#      html_static_path = ['_static']  # no static content required
