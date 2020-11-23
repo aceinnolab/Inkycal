058a5ab0631d3755aa5e5e13a8248b2e8ad70b2c
Ace <saadnaseer63@yahoo.de>
2020-11-24 00:35:12 +0100

    updated part of documentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = '2.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinxemoji.sphinxemoji',
-              'sphinx_rtd_theme']
+              'sphinx_rtd_theme', 'recommonmark']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -46,9 +47,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme" # html_theme = 'classic' # removed classic theme
+html_theme = "sphinx_rtd_theme" # html_theme = 'classic'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#      html_static_path = ['_static']  # no static content required
+# html_static_path = ['_static']
---
+++ docs/quickstart.md
@@ -0,0 +1,20 @@
+# Quickstart
+<img align="center" src="https://raw.githubusercontent.com/aceisace/Inky-Calendar/development/Gallery/logo.png" width="800">
+
+## Installing Inkycal
+
+Install inkycal  directly via the github repo:
+
+```bash
+# clone the repo
+git clone -b  release/2.0.0 https://github.com/aceisace/Inky-Calendar Inkycal
+
+# go to downloaded repo folder
+cd Inkycal
+
+# install Inkycal
+pip3 install -e ./
+```
+
+## Creating settings file
+coming soon
