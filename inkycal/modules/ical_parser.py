5e407f174b1b7dd4620443b6d4a1d08c23e32685
emilyboda <emily.boda@gmail.com>
2021-06-01 14:14:44 -0400

    simpler fix for identifying all day events
    This is a corrected version of PR #161 and includes PR #185

--- inkycal/modules/ical_parser.py
+++ inkycal/modules/ical_parser.py
@@ -18,6 +18,7 @@ from urllib.request import urlopen
 import logging
 import time
 import os
+from datetime import timedelta
 
 try:
   import recurring_ical_events
@@ -129,23 +130,29 @@ class iCalendar:
                         t_start_recurring, t_end_recurring)
                         for ical in self.icalendars)
 
-    events = (
-      {
-      'title': events.get('SUMMARY').lstrip(),
+    event_list = []
+    for ical in recurring_events:
+        for events in ical:
+            this_event = {}
+            this_event['title'] = events.get('SUMMARY').lstrip()
 
-      'begin': arrow.get(events.get('DTSTART').dt).to(timezone) if (
-        arrow.get(events.get('dtstart').dt).format('HH:mm') != '00:00')
-        else arrow.get(events.get('DTSTART').dt).replace(tzinfo=timezone),
+            starts_and_ends_at_midnight = False
+            ends_on_a_later_day_than_it_started = False
 
-      'end':arrow.get(events.get("DTEND").dt).to(timezone) if (
-        arrow.get(events.get('dtstart').dt).format('HH:mm') != '00:00')
-        else arrow.get(events.get('DTEND').dt).replace(tzinfo=timezone)
+            if arrow.get(events.get('dtstart').dt).format('HH:mm') == arrow.get(events.get('dtend').dt).format('HH:mm') == '00:00' and arrow.get(events.get('dtstart').dt) < arrow.get(events.get('dtend').dt):
+                # this is most likely an all-day event and we need to add timezone info to it
+                this_event['begin'] = arrow.get(events.get('DTSTART').dt).replace(tzinfo=timezone)
+                this_event['end'] = arrow.get(events.get('DTEND').dt).replace(tzinfo=timezone)
+            else:
+                # this is probably not an all-day event
+                # this might be a regular old event, or an event that starts at midnight UTC but doesn't end at midnight UTC
+                this_event['begin'] = arrow.get(events.get('DTSTART').dt).to(timezone)
+                this_event['end'] = arrow.get(events.get('DTEND').dt).to(timezone)
 
-      } for ical in recurring_events for events in ical)
-
+            event_list.append(this_event)
 
     # if any recurring events were found, add them to parsed_events
-    if events: self.parsed_events += list(events)
+    if events: self.parsed_events += list(event_list)
 
     # Sort events by their beginning date
     self.sort()
@@ -179,7 +186,7 @@ class iCalendar:
       begin, end = event['begin'], event['end']
       duration = end - begin
       if (begin.format('HH:mm') == '00:00' and end.format('HH:mm') == '00:00'
-          and duration.days >= 1):
+          and duration > timedelta(hours=22)):
         return True
       else:
         return False
