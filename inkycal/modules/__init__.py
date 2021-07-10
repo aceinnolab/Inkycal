46e365a67a500094a44f270f96201f3b8b3000b3
wface <schumi2012@gmail.com>
2021-07-10 21:54:34 +0200

    added xkcd_module

--- inkycal/modules/__init__.py
+++ inkycal/modules/__init__.py
@@ -7,4 +7,5 @@ from .inkycal_image import Inkyimage
 from .inkycal_jokes import Jokes
 from .inkycal_stocks import Stocks
 from .inkycal_slideshow import Slideshow
+from .inkycal_xkcd import Xkcd
 #from .inkycal_server import Inkyserver
