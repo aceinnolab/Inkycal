b65e59b6bbf3a332aefa89d28c84d39c4f8043c1
aceisace <saadnaseer63@yahoo.de>
2022-04-10 02:27:44 +0200

    modify parallel epd run file

---
+++ inkycal/display/drivers/parallel_drivers/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Unzip C++ SPI library and driver files
+echo "Unzipping files"
+unzip bcm2835-1.68.zip
+unzip IT8951.zip
+rm bcm2835-1.68.zip
+rm IT8951.zip
+
+# Install C++ SPI library for Raspberry
+echo "Installing C++ SPI library"
+cd bcm2835-1.68
+chmod +x configure
+./configure
+make
+sudo make check
+sudo make install
+
+# Install 7.8" E-Paper drivers
+echo "Installing 7.8 inch E-Paper drivers"
+cd ..
+cd IT8951
+sudo make clean
+sudo make -j4
+
+# Show image to check if it works
+echo "Showing demo image"
+sudo ./IT8951 0 0 pika.bmp
+
+echo "If you see a pikachu on the E-Paper, the install"
+echo "was successfull. If not, please report this issue."
