003258eb39eda9ad73821161797270b66c8b6612
Ace <saadnaseer63@yahoo.de>
2023-04-16 21:16:42 +0200

    temp disable Stocks module

--- inkycal/modules/__init__.py
+++ inkycal/modules/__init__.py
@@ -5,6 +5,6 @@ from .inkycal_feeds import Feeds
 from .inkycal_todoist import Todoist
 from .inkycal_image import Inkyimage
 from .inkycal_jokes import Jokes
-from .inkycal_stocks import Stocks
+#from .inkycal_stocks import Stocks
 from .inkycal_slideshow import Slideshow
 from .inkycal_textfile_to_display import TextToDisplay
