042dc92ffe40b587651ba447e289c133d64575d6
mrbwburns <>
2023-11-26 20:29:02 +0100

    bump devcontainer to bookworm, add documentation

--- run_weather_scraper.sh
+++ run_weather_scraper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 /home/ubuntu/Inkycal/inkycal/modules/inkycal_openweather_scrape.py 
+python3 /home/ubuntu/Inkycal/inkycal/modules/inkycal_openweather_scrape.py
 scp -i /home/ubuntu/.ssh/id_rsa /tmp/openweather_scraped.png inky@10.10.9.10:~/Inkycal/
