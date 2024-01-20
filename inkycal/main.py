7b396a8f138850f8c7932771b409fe85701fa6ec
mrbwburns <>
2024-01-20 15:02:35 +0100

    no optimization for 9.7 epd

--- inkycal/main.py
+++ inkycal/main.py
@@ -104,7 +104,7 @@ class Inkycal:
             os.mkdir(image_folder)
 
         # Option to use epaper image optimisation, reduces colours
-        self.optimize = True
+        self.optimize = False
 
         self.show_border = self.settings.get('border_around_modules', False)
 
