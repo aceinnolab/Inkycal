89417841f7b076f0fd3471290c841b602d1f497a
Ace <saadnaseer63@yahoo.de>
2024-07-05 13:20:57 +0200

    fix parallel drivers not finding the image

--- inkycal/display/drivers/7_in_8.py
+++ inkycal/display/drivers/7_in_8.py
@@ -34,8 +34,8 @@ class EPD:
     def getbuffer(self, image):
         """ad-hoc"""
         image = image.rotate(90, expand=True)
-        image.convert('RGB').save(os.path.join(settings.IMAGE_FOLDER, 'canvas.bmp'), 'BMP')
-        command = f'sudo {settings.PARALLEL_DRIVER_PATH}/epd -{settings.VCOM} 0 {settings.IMAGE_FOLDER + "canvas.bmp"}'
+        image.convert("RGB").save(os.path.join(settings.IMAGE_FOLDER, "canvas.bmp"), 'BMP')
+        command = f'sudo {settings.PARALLEL_DRIVER_PATH}/epd -{settings.VCOM} 0 {os.path.join(settings.IMAGE_FOLDER, "canvas.bmp")}'
         print(command)
         return command
 
